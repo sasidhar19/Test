d12e40884f0d74790c4445e9cc5fbd93bb5abdf9
Sasidhar <chsasidhar19@gmail.com>
2019-04-11 18:41:22 -0400

    message2

--- Project1/src/com/package1/Addition.java
+++ Project1/src/com/package1/Addition.java
@@ -2,9 +2,16 @@ package com.package1;
 
 public class Addition {
 	
+	public static void main(String[] args) {
+		Addition a = new Addition();
+		int x = a.m1(4,5);
+		System.out.println(x);
+	}
 	public int m1(int x, int y) {
 		
 		return x+y;
 	}
-
 }
+
+
+
